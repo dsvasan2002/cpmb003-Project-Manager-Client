447a20e6f4941bd607f23cf6921046f0ee8f42b9
dsvasan2002 <dsvasan2002@gmail.com>
2019-08-20 17:33:20 -0400

    updated host and ports

--- src/app/service/util.service.ts
+++ src/app/service/util.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UtilService {
-  readonly ROOT_URL: string = "http://localhost:3636/api";
+  readonly ROOT_URL: string = "http://localhost:3000/api";
 
   constructor() { }
 }
